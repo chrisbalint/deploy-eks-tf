8a2c26afa31ace44915f298f8bb3e48b4ca69a98
Cristian Balint <cristian.balint@spr.com>
2022-10-26 21:30:54 -0400

    delete resources

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
-resource "aws_s3_bucket" "test" {
-  bucket = "a-testbucket-77777"
+# resource "aws_s3_bucket" "test" {
+#   bucket = "a-testbucket-77777"
 
-  # tags = {
-  #   Name        = "My bucket"
-  #   Environment = "Dev"
-  # }
-  tags = var.tags
-}
+#   # tags = {
+#   #   Name        = "My bucket"
+#   #   Environment = "Dev"
+#   # }
+#   tags = var.tags
+# }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "a-allow-tls-test-7777"
-  description = "Allow TLS inbound traffic"
-  vpc_id = "vpc-069ab62413303dad8"
-  
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = var.cidr_blocks
-  }
-}
+# resource "aws_security_group" "allow_tls" {
+#   name        = "a-allow-tls-test-7777"
+#   description = "Allow TLS inbound traffic"
+#   vpc_id = "vpc-069ab62413303dad8"
+
+#   ingress {
+#     description      = "TLS from VPC"
+#     from_port        = 443
+#     to_port          = 443
+#     protocol         = "tcp"
+#     cidr_blocks      = var.cidr_blocks
+#   }
+# }
